79a41f193a9193a06737a1c932b6aef4c3398226
Sergey Abramov <s.abramov@glowbyte.io>
2025-06-21 12:15:52 +0300

    fish updates

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,7 @@ abbr ldocker lazydocker
 abbr venv . .venv/bin/activate.fish
 
 alias k9s "TERM=xterm-truecolor /opt/homebrew/bin/k9s"
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
